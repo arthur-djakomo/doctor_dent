340a36112363ff4f697ae847c815200dfd39a4b3
arthur-djakomo <58634631+arthur-djakomo@users.noreply.github.com>
2020-02-17 08:33:18 +0200

    Add files via upload
    jquery, javascript, slider - slick

---
+++ doctor_dent/js/my.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
+        
+    });
+    
+    
+    
+    });
